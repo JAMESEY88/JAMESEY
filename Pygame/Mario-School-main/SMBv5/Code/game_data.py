85ff044a9d808da91c69869d35b6f84a11f9658b
JAMESEY88 <131949100+JAMESEY88@users.noreply.github.com>
2023-05-03 16:19:13 +1000

    Add files via upload

---
+++ Pygame/Mario-School-main/SMBv5/Code/game_data.py
@@ -0,0 +1,10 @@
+level_1 = {
+        'terrain': 'C:/Users/61430/Desktop/ASSIGNMENT/Pygame/Mario-School-main/SMBv5/Levels/1/level_1_terrain.csv',
+        'background': 'C:/Users/61430/Desktop/ASSIGNMENT/Pygame/Mario-School-main/SMBv5/Levels/1/level_1_background.csv',
+        'constraints': 'C:/Users/61430/Desktop/ASSIGNMENT/Pygame/Mario-School-main/SMBv5/Levels/1/level_1_constraints.csv',
+        'enemies': 'C:/Users/61430/Desktop/ASSIGNMENT/Pygame/Mario-School-main/SMBv5/Levels/1/level_1_enemy.csv',
+        'player': 'C:/Users/61430/Desktop/ASSIGNMENT/Pygame/Mario-School-main/SMBv5/Levels/1/level_1_player.csv',
+        'blocks': 'C:/Users/61430/Desktop/ASSIGNMENT/Pygame/Mario-School-main/SMBv5/Levels/1/level_1_blocks.csv',
+        'flag': 'C:/Users/61430/Desktop/ASSIGNMENT/Pygame/Mario-School-main/SMBv5/Levels/1/level_1_flag.csv',
+        'flag_pole': 'C:/Users/61430/Desktop/ASSIGNMENT/Pygame/Mario-School-main/SMBv5/Levels/1/level_1_flag_pole.csv',
+        'house' :'C:/Users/61430/Desktop/ASSIGNMENT/Pygame/Mario-School-main/SMBv5/Levels/1/level_1_house.csv'}
